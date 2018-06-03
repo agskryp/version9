1b87d25be7cf92deaaaca42710d0c2423de5ee07
Andrew Skrypnyk <a.g.skryp@gmail.com>
2018-06-03 00:03:18 -0400

    Push header to left column, content on the right

--- footer.php
+++ footer.php
@@ -1,17 +1,14 @@
 <?php
-/**
- * The template for displaying the footer
- *
- * Contains the closing of the #content div and all content after.
- *
- * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
- *
- * @package version9
- */
-
+  /**
+   * The template for displaying the footer
+   * Contains the closing of the #content div and all content after.
+   *
+   * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+   * @package version9
+   */
 ?>
 
-	</div><!-- #content -->
+</div><!-- #content -->
 
 	<footer id="colophon" class="site-footer">
 		<div class="site-info">
@@ -28,6 +25,7 @@
 				?>
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
+</div> <!-- bootstrap .row -->
 </div><!-- #page -->
 
 <?php wp_footer(); ?>
