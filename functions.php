64565557df49cbc80d8147ce8ef8ac028ae74e81
Andrew Skrypnyk <a.g.skryp@gmail.com>
2018-06-03 11:58:14 -0400

    Fixed full screen background

--- functions.php
+++ functions.php
@@ -123,13 +123,12 @@ function version9_scripts() {
   wp_enqueue_style( 'bootstrap-v4.1', get_template_directory_uri() . '/css/bootstrap.min.css', array(), '4.1.0', false );
   wp_enqueue_style( 'version9-style', get_stylesheet_uri() );
 
-	wp_enqueue_script( 'version9-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20151215', true );
+  wp_enqueue_script( 'version9-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20151215', true );
+  wp_enqueue_script( 'version9-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20151215', true );
 
-	wp_enqueue_script( 'version9-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20151215', true );
-
-	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
-		wp_enqueue_script( 'comment-reply' );
-	}
+  if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
+    wp_enqueue_script( 'comment-reply' );
+  }
 }
 add_action( 'wp_enqueue_scripts', 'version9_scripts' );
 
