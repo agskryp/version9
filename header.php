88804d5c111f36134a20425a8ae2b6471825c5f6
Andrew Skrypnyk <a.g.skryp@gmail.com>
2018-06-06 16:54:17 -0400

    Combine search and menu into nav element

--- header.php
+++ header.php
@@ -47,26 +47,28 @@
           </div>
 		</div>
 
-		<nav id="site-navigation" class="main-navigation"> 
-          <button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false">
-            <?php esc_html_e( 'Hamburger Menu', 'version9' ); ?>
-          </button>
-          
-          <button class="menu-toggle" aria-controls="search-form" aria-expanded="false">
-            <?php esc_html_e( 'Search', 'version9' ); ?>
-          </button>
-		
-          <?php
-			wp_nav_menu( array(
-		      'theme_location' => 'menu-1',
-		      'menu_id'        => 'primary-menu',
-			) );
-		  ?>
-		</nav> 
-        
-        <div id="site-search" class="main-search-form">
-          <?php get_search_form( ); ?>
-        </div>
+		<nav>
+          <div id="site-navigation" class="main-navigation"> 
+            <button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false">
+              <?php esc_html_e( 'Menu', 'version9' ); ?>
+            </button>
+
+            <button class="menu-toggle" aria-controls="search-form" aria-expanded="false">
+              <?php esc_html_e( 'Search', 'version9' ); ?>
+            </button>
+
+            <?php
+              wp_nav_menu( array(
+                'theme_location' => 'menu-1',
+                'menu_id'        => 'primary-menu',
+              ) );
+            ?>
+          </div> 
+
+          <div id="site-search" class="main-search-form">
+            <?php get_search_form( ); ?>
+          </div>
+        </nav>
         
           
       </header> <?php // #masthead // ?>
