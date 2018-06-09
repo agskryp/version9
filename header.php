de2503ebe6e8b88d7158819323d5c8f2bdc9f18b
Andrew Skrypnyk <a.g.skryp@gmail.com>
2018-06-09 14:30:48 -0400

    Clean code and remove inline styling

--- header.php
+++ header.php
@@ -44,29 +44,26 @@
         </div>
 
         <nav id="site-navigation" class="site-navigation">
-          <div class="main-buttons" style="display: inline-flex;
-    flex-direction: column-reverse;
-    float: right;
-    margin-top: -64px;">
-          <button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false">
-            <span>
-              <?php esc_html_e( 'Menu', 'version9' ); ?>
-            </span>
-            
-            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
-              <path d="M24 6h-24v-4h24v4zm0 4h-24v4h24v-4zm0 8h-24v4h24v-4z"/>
-            </svg>
-          </button>
+          <div class="main-nav-button">
+            <button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false">
+              <span>
+                <?php esc_html_e( 'Menu', 'version9' ); ?>
+              </span>
 
-          <button class="menu-toggle" aria-controls="search-form" aria-expanded="false">
-            <span>
-              <?php esc_html_e( 'Search', 'version9' ); ?>
-            </span>
-            
-            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
-              <path d="M23.111 20.058l-4.977-4.977c.965-1.52 1.523-3.322 1.523-5.251 0-5.42-4.409-9.83-9.829-9.83-5.42 0-9.828 4.41-9.828 9.83s4.408 9.83 9.829 9.83c1.834 0 3.552-.505 5.022-1.383l5.021 5.021c2.144 2.141 5.384-1.096 3.239-3.24zm-20.064-10.228c0-3.739 3.043-6.782 6.782-6.782s6.782 3.042 6.782 6.782-3.043 6.782-6.782 6.782-6.782-3.043-6.782-6.782zm2.01-1.764c1.984-4.599 8.664-4.066 9.922.749-2.534-2.974-6.993-3.294-9.922-.749z"/>
-            </svg>
-          </button>
+              <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+                <path d="M24 6h-24v-4h24v4zm0 4h-24v4h24v-4zm0 8h-24v4h24v-4z"/>
+              </svg>
+            </button>
+
+            <button class="menu-toggle" aria-controls="search-form" aria-expanded="false">
+              <span>
+                <?php esc_html_e( 'Search', 'version9' ); ?>
+              </span>
+
+              <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+                <path d="M23.111 20.058l-4.977-4.977c.965-1.52 1.523-3.322 1.523-5.251 0-5.42-4.409-9.83-9.829-9.83-5.42 0-9.828 4.41-9.828 9.83s4.408 9.83 9.829 9.83c1.834 0 3.552-.505 5.022-1.383l5.021 5.021c2.144 2.141 5.384-1.096 3.239-3.24zm-20.064-10.228c0-3.739 3.043-6.782 6.782-6.782s6.782 3.042 6.782 6.782-3.043 6.782-6.782 6.782-6.782-3.043-6.782-6.782zm2.01-1.764c1.984-4.599 8.664-4.066 9.922.749-2.534-2.974-6.993-3.294-9.922-.749z"/>
+              </svg>
+            </button>
           </div>
 
           <div id="main-navigation" class="main-navigation">
@@ -79,17 +76,7 @@
           </div>
 
           <div id="site-search" class="main-search-form">
-            <form role="search" method="get" class="search-form" action="<?php echo home_url( '/' ); ?>">
-              <label>
-                  <span class="screen-reader-text">
-                    Search for:
-                </span>
-                
-                <input type="search" class="search-field" placeholder="Search" value="" name="s" title="Search for:" />
-              </label>
-              
-              <input type="submit" class="search-submit" value="Go" />
-            </form>
+            <?php require get_template_directory() . '/searchform.php';  ?>
           </div>
         </nav>
       </header>
