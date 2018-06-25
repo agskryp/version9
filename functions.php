77cd6e2c63687b35f7a3562ea55d9dd12d60ea00
Andrew Skrypnyk <a.g.skryp@gmail.com>
2018-06-24 20:47:02 -0400

    Include custom comment template

--- functions.php
+++ functions.php
@@ -172,3 +172,8 @@ remove_action( 'wp_head', 'print_emoji_detection_script', 7 );
 remove_action( 'wp_print_styles', 'print_emoji_styles' );
 remove_action( 'admin_print_scripts', 'print_emoji_detection_script' );
 remove_action( 'admin_print_styles', 'print_emoji_styles' );
+
+/**
+ * Custom template for comment section
+ */
+require get_template_directory() . '/inc/template-comments.php';
