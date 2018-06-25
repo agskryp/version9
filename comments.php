ddcebdbb866b22ec4fd079846b4ee78efc85e80b
Andrew Skrypnyk <a.g.skryp@gmail.com>
2018-06-25 15:49:26 -0400

    Use static title for comments

--- comments.php
+++ comments.php
@@ -21,52 +21,35 @@
 
 <div id="comments" class="comments-area">
   <?php	if ( have_comments() ) : ?>
-    <h2 class="comments-title">
+    <h2 class="comments-title text-center font-weight-bold">
+      Comments
+    </h2>
+
+    <?php the_comments_navigation(); ?>
+
+    <ol class="comment-list">
       <?php
-		$version9_comment_count = get_comments_number();
-      
-        if ( '1' === $version9_comment_count ) {
-		  printf(
-            /* translators: 1: title. */
-			esc_html__( 'One thought on &ldquo;%1$s&rdquo;', 'version9' ), '<span>' . get_the_title() . '</span>'
-          );
-        }
-        
-        else {
-		  printf( // WPCS: XSS OK.
-		    /* translators: 1: comment count number, 2: title. */
-		    esc_html( _nx( '%1$s thought on &ldquo;%2$s&rdquo;', '%1$s thoughts on &ldquo;%2$s&rdquo;', $version9_comment_count, 'comments title', 'version9' ) ),
-		    number_format_i18n( $version9_comment_count ), '<span>' . get_the_title() . '</span>'
-		    );
-		  }
-        ?>
-      </h2>
-
-      <?php the_comments_navigation(); ?>
-
-      <ol class="comment-list">
-        <?php
-		  wp_list_comments( array(
-		    'style'       => 'ol',
-		    'short_ping'  => true,
-            'avatar_size' => 64,
-            'walker'      => new version9_Walker_Comment()
-		  ) );
-		?>
-      </ol>
-
-      <?php
-		the_comments_navigation();
-
-		// If comments are closed and there are comments, let's leave a little note, shall we?
-		if ( ! comments_open() ) :
+        wp_list_comments( array(
+          'style'       => 'ol',
+          'short_ping'  => true,
+          'avatar_size' => 64,
+          'walker'      => new version9_Walker_Comment()
+        ) );
       ?>
-        <p class="no-comments">
-          <?php esc_html_e( 'Comments are closed.', 'version9' ); ?>
-        </p>
-  
-      <?php
-		endif;
+    </ol>
+
+    <?php
+      the_comments_navigation();
+
+      // If comments are closed and there are comments, let's leave a little note, shall we?
+      if ( ! comments_open() ) :
+    ?>
+      <p class="no-comments text-center font-weight-bold font-italic">
+        <?php esc_html_e( 'Comments are closed.', 'version9' ); ?>
+      </p>
+
+    <?php
+      endif;
 
 	endif; // Check for have_comments().
 
