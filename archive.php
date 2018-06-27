ae1be2fe552550f23072c95b50274fc2a0e0b69b
Andrew Skrypnyk <a.g.skryp@gmail.com>
2018-06-27 01:14:44 -0400

    Use custom archive title function

--- archive.php
+++ archive.php
@@ -17,7 +17,7 @@
 
       <header class="page-header text-center">
         <?php
-          the_archive_title( '<h1 class="page-title">', '</h1>' );
+          version9_archive_title( '<h1 class="page-title">', '</h1>' );
 
           the_archive_description( '<div class="archive-description">', '</div>' );
         ?>
--- inc/template-tags.php
+++ inc/template-tags.php
@@ -152,6 +152,136 @@ function version9_blog_navigation( $args = array() ) {
 
 
 
+/**
+ * Display the archive title based on the queried object.
+ */
+function version9_archive_title( $before = '', $after = '' ) {
+  $title = get_the_version9_archive_title();
+
+  if ( ! empty( $title ) ) {
+    echo $before . $title . $after;
+  }
+}
+
+/**
+ * Retrieve the archive title based on the queried object.
+ */
+function get_the_version9_archive_title() {
+	if ( is_category() ) {
+		/* translators: Category archive title. 1: Category name */
+		$title = sprintf( __( '%s' ), single_cat_title( '', false ) );
+	} elseif ( is_tag() ) {
+		/* translators: Tag archive title. 1: Tag name */
+		$title = sprintf( __( 'Tag: %s' ), single_tag_title( '', false ) );
+	} elseif ( is_author() ) {
+		/* translators: Author archive title. 1: Author name */
+		$title = sprintf( __( 'Author: %s' ), '<span class="vcard">' . get_the_author() . '</span>' );
+	} elseif ( is_year() ) {
+		/* translators: Yearly archive title. 1: Year */
+		$title = sprintf( __( 'Year: %s' ), get_the_date( _x( 'Y', 'yearly archives date format' ) ) );
+	} elseif ( is_month() ) {
+		/* translators: Monthly archive title. 1: Month name and year */
+		$title = sprintf( __( 'Month: %s' ), get_the_date( _x( 'F Y', 'monthly archives date format' ) ) );
+	} elseif ( is_day() ) {
+		/* translators: Daily archive title. 1: Date */
+		$title = sprintf( __( 'Day: %s' ), get_the_date( _x( 'F j, Y', 'daily archives date format' ) ) );
+	} elseif ( is_tax( 'post_format' ) ) {
+		if ( is_tax( 'post_format', 'post-format-aside' ) ) {
+			$title = _x( 'Asides', 'post format archive title' );
+		} elseif ( is_tax( 'post_format', 'post-format-gallery' ) ) {
+			$title = _x( 'Galleries', 'post format archive title' );
+		} elseif ( is_tax( 'post_format', 'post-format-image' ) ) {
+			$title = _x( 'Images', 'post format archive title' );
+		} elseif ( is_tax( 'post_format', 'post-format-video' ) ) {
+			$title = _x( 'Videos', 'post format archive title' );
+		} elseif ( is_tax( 'post_format', 'post-format-quote' ) ) {
+			$title = _x( 'Quotes', 'post format archive title' );
+		} elseif ( is_tax( 'post_format', 'post-format-link' ) ) {
+			$title = _x( 'Links', 'post format archive title' );
+		} elseif ( is_tax( 'post_format', 'post-format-status' ) ) {
+			$title = _x( 'Statuses', 'post format archive title' );
+		} elseif ( is_tax( 'post_format', 'post-format-audio' ) ) {
+			$title = _x( 'Audio', 'post format archive title' );
+		} elseif ( is_tax( 'post_format', 'post-format-chat' ) ) {
+			$title = _x( 'Chats', 'post format archive title' );
+		}
+	} elseif ( is_post_type_archive() ) {
+		/* translators: Post type archive title. 1: Post type name */
+		$title = sprintf( __( 'Archives: %s' ), post_type_archive_title( '', false ) );
+	} elseif ( is_tax() ) {
+		$tax = get_taxonomy( get_queried_object()->taxonomy );
+		/* translators: Taxonomy term archive title. 1: Taxonomy singular name, 2: Current taxonomy term */
+		$title = sprintf( __( '%1$s: %2$s' ), $tax->labels->singular_name, single_term_title( '', false ) );
+	} else {
+		$title = __( 'Archives' );
+	}
+
+	/* Filters the archive title. */
+	return apply_filters( 'get_the_version9_archive_title', $title );
+}
+
+/**
+ * Display category, tag, term, or author description.
+ *
+ * @since 4.1.0
+ *
+ * @see get_the_archive_description()
+ *
+ * @param string $before Optional. Content to prepend to the description. Default empty.
+ * @param string $after  Optional. Content to append to the description. Default empty.
+ */
+//function the_archive_description( $before = '', $after = '' ) {
+//	$description = get_the_archive_description();
+//	if ( $description ) {
+//		echo $before . $description . $after;
+//	}
+//}
+
+/**
+ * Retrieves the description for an author, post type, or term archive.
+ *
+ * @since 4.1.0
+ * @since 4.7.0 Added support for author archives.
+ * @since 4.9.0 Added support for post type archives.
+ *
+ * @see term_description()
+ *
+ * @return string Archive description.
+ */
+//function get_the_archive_description() {
+//	if ( is_author() ) {
+//		$description = get_the_author_meta( 'description' );
+//	} elseif ( is_post_type_archive() ) {
+//		$description = get_the_post_type_description();
+//	} else {
+//		$description = term_description();
+//	}
+
+	/**
+	 * Filters the archive description.
+	 *
+	 * @since 4.1.0
+	 *
+	 * @param string $description Archive description to be displayed.
+	 */
+//	return apply_filters( 'get_the_archive_description', $description );
+//}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 if ( ! function_exists( 'version9_posted_on' ) ) :
