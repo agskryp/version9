9372e22068809e5057d94a029aa04b4b5e53c959
Andrew Skrypnyk <a.g.skryp@gmail.com>
2018-06-28 22:09:57 -0400

    Template style wip

--- template-parts/content-none.php
+++ template-parts/content-none.php
@@ -1,51 +1,52 @@
 <?php
-/**
- * Template part for displaying a message that posts cannot be found
- *
- * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
- *
- * @package version9
- */
-
+  /**
+   * Template part for displaying a message that posts cannot be found
+   *
+   * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+   * @package version9
+   */
 ?>
 
 <section class="no-results not-found">
-	<header class="page-header">
-		<h1 class="page-title"><?php esc_html_e( 'Nothing Found', 'version9' ); ?></h1>
-	</header><!-- .page-header -->
+  <header class="page-header">
+    <h1 class="page-title"><?php esc_html_e( 'Nothing Found', 'version9' ); ?></h1>
+  </header>
 
-	<div class="page-content">
-		<?php
-		if ( is_home() && current_user_can( 'publish_posts' ) ) :
+  <div class="page-content">
+    <?php
+      if ( is_home() && current_user_can( 'publish_posts' ) ) :
+        printf( '<p>' . wp_kses(
+          /* translators: 1: link to WP admin new post page. */
+          __( 'Ready to publish your first post? <a href="%1$s">Get started here</a>.', 'version9' ),
+          array(
+            'a' => array(
+              'href' => array(),
+          ), )
+        ) . '</p>', esc_url( admin_url( 'post-new.php' ) ) );
 
-			printf(
-				'<p>' . wp_kses(
-					/* translators: 1: link to WP admin new post page. */
-					__( 'Ready to publish your first post? <a href="%1$s">Get started here</a>.', 'version9' ),
-					array(
-						'a' => array(
-							'href' => array(),
-						),
-					)
-				) . '</p>',
-				esc_url( admin_url( 'post-new.php' ) )
-			);
+      elseif ( is_search() ) :
+    ?>
+      
+      <p>
+        Sorry, but nothing matched your search terms. Please try again with some different keywords.        
+      </p>
+        
+    <div id="site-search" class="main-search-form">
+            <?php require get_template_directory() . '/searchform.php';  ?>
+          </div>
+<?php
+      else :
+    ?>
 
-		elseif ( is_search() ) :
-			?>
-
-			<p><?php esc_html_e( 'Sorry, but nothing matched your search terms. Please try again with some different keywords.', 'version9' ); ?></p>
-			<?php
-			get_search_form();
-
-		else :
-			?>
-
-			<p><?php esc_html_e( 'It seems we can&rsquo;t find what you&rsquo;re looking for. Perhaps searching can help.', 'version9' ); ?></p>
-			<?php
-			get_search_form();
-
-		endif;
-		?>
-	</div><!-- .page-content -->
-</section><!-- .no-results -->
+      <p>
+        It seems we can&rsquo;t find what you&rsquo;re looking for. Perhaps searching can help.
+      </p>
+    
+    <div id="site-search" class="main-search-form">
+            <?php require get_template_directory() . '/searchform.php';  ?>
+          </div>
+<?php
+      endif;
+    ?>
+  </div>
+</section>
