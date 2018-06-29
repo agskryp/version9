bd9465b21d5ddaf820856805cc1687c4226e6eda
Andrew Skrypnyk <a.g.skryp@gmail.com>
2018-06-28 22:10:22 -0400

    Style and code clean up

--- search.php
+++ search.php
@@ -1,55 +1,63 @@
 <?php
-/**
- * The template for displaying search results pages
- *
- * @link https://developer.wordpress.org/themes/basics/template-hierarchy/#search-result
- *
- * @package version9
- */
+  /**
+   * The template for displaying search results pages
+   *
+   * @link https://developer.wordpress.org/themes/basics/template-hierarchy/#search-result
+   * @package version9
+   */
 
-get_header();
+  get_header();
 ?>
 
-	<section id="primary" class="content-area">
-		<main id="main" class="site-main">
+<div id="primary">
+  <main id="main" class="site-main search-page">
+    <?php if ( have_posts() ) : ?>
+      <header class="page-header">
+        <h1 class="page-title text-center">
+          <?php
+            global $wp_query;
+            $number_of_results = $wp_query->found_posts;
+            
+            /* translators: %s: search query. */
+            if ( $number_of_results == 1 ) {
+              printf( esc_html__( 'There is ' . $number_of_results . ' result for %s', 'version9' ),
+              '<span class="search-result">' . get_search_query() . '</span>' );
+            }
 
-		<?php if ( have_posts() ) : ?>
+            else {
+              printf( esc_html__( 'There are ' . $number_of_results . ' results for %s', 'version9' ),
+              '<span class="search-result">' . get_search_query() . '</span>' );
+            }
+          ?>
+        </h1>
+      </header>
 
-			<header class="page-header">
-				<h1 class="page-title">
-					<?php
-					/* translators: %s: search query. */
-					printf( esc_html__( 'Search Results for: %s', 'version9' ), '<span>' . get_search_query() . '</span>' );
-					?>
-				</h1>
-			</header><!-- .page-header -->
-
-			<?php
-			/* Start the Loop */
-			while ( have_posts() ) :
-				the_post();
-
-				/**
-				 * Run the loop for the search to output the results.
-				 * If you want to overload this in a child theme then include a file
-				 * called content-search.php and that will be used instead.
-				 */
-				get_template_part( 'template-parts/content', 'search' );
-
-			endwhile;
-
-			the_posts_navigation();
-
-		else :
-
-			get_template_part( 'template-parts/content', 'none' );
-
-		endif;
-		?>
-
-		</main><!-- #main -->
       <?php
-      get_sidebar();
-      get_footer();
-      ?>
-	</section><!-- #primary -->
+        /* Start the Loop */
+        while ( have_posts() ) :
+          the_post();
+
+          /**
+           * Run the loop for the search to output the results.
+           * If you want to overload this in a child theme then include a file
+           * called content-search.php and that will be used instead.
+           */
+          get_template_part( 'template-parts/content', 'search' );
+
+        endwhile;
+
+        the_posts_navigation();
+
+      else :
+        get_template_part( 'template-parts/content', 'none' );
+
+      endif;
+    ?>
+  </main>
+  
+  <?php
+    get_sidebar();
+  
+    get_footer();
+  ?>
+</div>
