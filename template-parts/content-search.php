c943022bb4ea3cb1e749989f89ef7a53cf532ada
Andrew Skrypnyk <a.g.skryp@gmail.com>
2018-06-28 22:09:43 -0400

    Style template and clean code

--- template-parts/content-search.php
+++ template-parts/content-search.php
@@ -1,35 +1,37 @@
 <?php
-/**
- * Template part for displaying results in search pages
- *
- * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
- *
- * @package version9
- */
-
+  /**
+   * Template part for displaying results in search pages
+   *
+   * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+   * @package version9
+   */
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<header class="entry-header">
-		<?php the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' ); ?>
+  <header class="entry-header">
+    <?php
+      the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' );
+    
+      if ( 'post' === get_post_type() ) :
+    ?>
+    
+      <div class="entry-meta">
+        <?php version9_post_header_meta(); ?>
+      </div>
+    <?php endif; ?>
+  </header>
 
-		<?php if ( 'post' === get_post_type() ) : ?>
-		<div class="entry-meta">
-			<?php
-			version9_posted_on();
-			version9_posted_by();
-			?>
-		</div><!-- .entry-meta -->
-		<?php endif; ?>
-	</header><!-- .entry-header -->
+  <?php if ( has_post_thumbnail() ) : ?>
+    <div class="text-center featured-image-container">
+      <?php version9_post_thumbnail(); ?>
+    </div>
+  <?php endif; ?>
 
-	<?php version9_post_thumbnail(); ?>
+  <div class="entry-summary">
+    <?php the_excerpt(); ?>
+  </div>
 
-	<div class="entry-summary">
-		<?php the_excerpt(); ?>
-	</div><!-- .entry-summary -->
-
-	<footer class="entry-footer">
-		<?php version9_entry_footer(); ?>
-	</footer><!-- .entry-footer -->
-</article><!-- #post-<?php the_ID(); ?> -->
+  <footer class="entry-footer">
+    <?php version9_post_footer_meta(); ?>
+  </footer>
+</article>
