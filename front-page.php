b4452bfb4aaed31a68857a427734fc59bbb131de
Andrew Skrypnyk <a.g.skryp@gmail.com>
2018-06-30 14:42:48 -0400

    Code and styling wip

--- front-page.php
+++ front-page.php
@@ -3,117 +3,67 @@
    * The front page file.
    *
    * @link https://codex.wordpress.org/Template_Hierarchy
-   * @package AG_Theme
+   * @package version9
    */
+
+  require get_template_directory() . '/partials/doctype.php';
 ?>
 
-<!DOCTYPE html>
-<html <?php language_attributes(); ?> class="homepage-bkgd">
+<div id="page" class="homepage-screen">
+  <a class="skip-link screen-reader-text">
+    <?php esc_html_e( 'Skip to menu', 'version9' ); ?>
+  </a>
 
-<head>
-  <meta charset="<?php bloginfo( 'charset' ); ?>">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="profile" href="http://gmpg.org/xfn/11">
+  <div id="primary" class="content-area">
+    <header>
+      <?php require get_template_directory() . '/partials/site-branding.php'; ?>
+    </header>
 
-  <?php wp_head(); ?>
-</head>
-
-<body <?php body_class(); ?>>
-  <div id="page" class="homepage-screen">
-    <a class="skip-link screen-reader-text" href="#homepageMenu">
-      <?php esc_html_e( 'Skip to menu', 'agtheme' ); ?>
-    </a>
-
-    <div id="primary" class="content-area">
-      <header class="site-branding">
-        <h1 class="site-title">
-          <a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">
-            <span>
-              Andrew
-            </span>
-              
-            <span>
-              Skrypnyk
-            </span>
-          </a>
-        </h1>
-
-        <div class="site-description">
-            <p class="text-center">
-              <span>
-                Web Design
-              </span>
-              
-              <span>
-                &amp; Development
-              </span>
+    <main id="main" class="site-main" role="main">
+      <ul id="homepageMenu" class="homepage-container">
+        <li class="homepage-menu-item">
+          <a href="/blog">
+            <p>
+              Browse through articles and writings about anything front-end related
             </p>
-          </div>
-      </header>
+            
+            <button class="homepage-menu-button">
+              Read Blog
+            </button> 
+          </a>
+        </li>
 
+        <li class="homepage-menu-item">
+          <a href="/portfolio">
+            <p>
+              Check out various web projects and what was involved in the building process
+            </p>
+            
+            <button class="homepage-menu-button">
+              View Portfolio
+            </button>
+          </a>
+        </li>
 
-      
-      
-      
-      
-      
-      
-      
-      
-      
-      
-      
-      
-      
-      
-      
-      
-      
-      <main id="main" class="site-main" role="main">
-        <ul id="homepageMenu" class="homepage-container">
-          <li class="homepage-menu-item homepage-menu-item-blog">
-            <a href="/blog">
-                              <i class="fa fa-newspaper-o fa-5x"></i>
-                              <p>Browse through articles and writings about anything front-end related</p>
-                              <div class="homepage-menu-button">Read Blog</div> 
-                          </a>
-          </li>
+        <li class="homepage-menu-item">
+          <a href="/about">
+            <p>
+              Learn more about the man behind the page in front of the computer beyond the internet.
+            </p>
+            
+            <div class="homepage-menu-button">
+              About Me
+            </div>
+          </a>
+        </li>
+      </ul>
+    </main>      <!-- #main -->
+  </div>    <!-- #primary -->
+</div>  <!-- #page -->
 
-          <li class="homepage-menu-item homepage-menu-item-portfolio">
-            <a href="/portfolio">
-                              <i class="fa fa-list-alt fa-5x"></i>
-                              <p>Check out various web projects and what was involved in the building process</p>
-                              <div class="homepage-menu-button">View Portfolio</div>
-                          </a>
-          </li>
+<script type='text/javascript' src="https://cdnjs.cloudflare.com/ajax/libs/gsap/1.19.0/TweenMax.min.js"></script>
 
-          <li class="homepage-menu-item homepage-menu-item-about">
-            <a href="/about">
-                              <i class="fa fa-user fa-5x"></i>
-                              <p>Learn more about the man behind the page in front of the computer beyond the internet.</p>
-                              <div class="homepage-menu-button">About Me</div>
-                          </a>
-          </li>
-
-
-          <li class="homepage-menu-item homepage-menu-item-contact">
-            <a href="/contact">
-                              <i class="fa fa-envelope fa-5x"></i>
-                              <p>Connect with a social network or send a message with an easy to use form</p>
-                              <div class="homepage-menu-button">Contact Me</div>
-                          </a>
-          </li>
-        </ul>
-      </main>
-      <!-- #main -->
-    </div>
-    <!-- #primary -->
-  </div>
-  <!-- #page -->
-
-  <script type='text/javascript' src="https://cdnjs.cloudflare.com/ajax/libs/gsap/1.19.0/TweenMax.min.js"></script>
-
-  <?php wp_footer(); ?>
+<?php wp_footer(); ?>
 
 </body>
 
