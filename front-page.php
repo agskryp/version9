0e4b43a93ddb60b5148da1bb6a778c4ea86861c6
Andrew Skrypnyk <a.g.skryp@gmail.com>
2018-07-02 16:09:56 -0400

    Content and style touch ups

--- front-page.php
+++ front-page.php
@@ -22,7 +22,7 @@
     <main id="main" class="site-main" role="main">
       <ul id="homepageMenu" class="homepage-container">
         <li class="homepage-menu-item">
-          <svg title="Blog" xmlns="http://www.w3.org/2000/svg" width="56" height="56" viewBox="0 0 24 24">
+          <svg xmlns="http://www.w3.org/2000/svg" width="56" height="56" viewBox="0 0 24 24">
             <path d="M15 12h-10v1h10v-1zm-4 2h-6v1h6v-1zm4-6h-10v1h10v-1zm0 2h-10v1h10v-1zm0-6h-10v1h10v-1zm0 2h-10v1h10v-1zm7.44 10.277c.183-2.314-.433-2.54-3.288-5.322.171 1.223.528 3.397.911 5.001.089.382-.416.621-.586.215-.204-.495-.535-2.602-.82-4.72-.154-1.134-1.661-.995-1.657.177.005 1.822.003 3.341 0 6.041-.003 2.303 1.046 2.348 1.819 4.931.132.444.246.927.339 1.399l3.842-1.339c-1.339-2.621-.693-4.689-.56-6.383zm-6.428 1.723h-13.012v-16h14v7.894c.646-.342 1.348-.274 1.877.101l.123-.018v-8.477c0-.828-.672-1.5-1.5-1.5h-15c-.828 0-1.5.671-1.5 1.5v17c0 .829.672 1.5 1.5 1.5h13.974c-.245-.515-.425-1.124-.462-2z"/>
             
             <h3 class="sr-only">
@@ -31,7 +31,7 @@
           </svg>
 
           <p>
-            Browse through articles and writings about anything front-end related
+            Browse through tutorials, blog posts, and writings about web development
           </p>
 
           <a href="<?php echo esc_url( __( AG_BLOG, 'version9' ) ); ?>">
@@ -49,11 +49,11 @@
           </svg>
             
           <p>
-            Check out various web projects and what was involved in the building process
+            Check out various web projects and my involvment in the building process
           </p>
             
           <a href="<?php echo esc_url( __( AG_PORTFOLIO, 'version9' ) ); ?>">
-            View Portfolio
+            View Works
           </a>
         </li>
 
@@ -67,15 +67,15 @@
           </svg>
 
           <p>
-            Learn more about the man behind the page in front of the computer beyond the internet.
+            Learn more about the man behind the page in front of a computer beyond the internet
           </p>
 
           <a href="<?php echo esc_url( __( AG_BIO, 'version9' ) ); ?>">
-            About Me
+            View Bio
           </a>
         </li>
       </ul>
-    </main>      <!-- #main -->
+    </main>
   </div>    <!-- #primary -->
 </div>  <!-- #page -->
 
