6b871b74a83875384de6157cb05a59c4c7e1ccf3
Andrew Skrypnyk <a.g.skryp@gmail.com>
2018-07-03 18:17:10 -0400

    Don't load blog navigation on portfolio entries

--- single.php
+++ single.php
@@ -14,16 +14,22 @@
     <?php
 	  while ( have_posts() ) :
         the_post();
+    
+        if ( get_post_type() === 'portfolio' ) {
+          get_template_part( 'template-parts/content', 'portfolio' );
+        }
+    
+        else {
+          get_template_part( 'template-parts/content', get_post_type() );
 
-		get_template_part( 'template-parts/content', get_post_type() );
+          version9_blog_navigation();
 
-		version9_blog_navigation();
-
-		// If comments are open or we have at least one comment, load up the comment template.
-		if ( comments_open() || get_comments_number() ) :
-		  comments_template();
-		endif;
-
+          // If comments are open or we have at least one comment, load up the comment template.
+          if ( comments_open() || get_comments_number() ) :
+            comments_template();
+          endif;
+        }
+    
       endwhile; // End of the loop.
 	?>
   </main>
