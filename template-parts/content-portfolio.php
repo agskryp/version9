27d7dec4a2d91ab5b4e001bc5b748ae522447f20
Andrew Skrypnyk <a.g.skryp@gmail.com>
2018-07-06 21:56:05 -0400

    Portfolio content and styling

--- template-parts/content-portfolio.php
+++ template-parts/content-portfolio.php
@@ -11,23 +11,38 @@
   <header class="entry-header">
 	<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
   </header>
-   
-  <div class="text-center featured-image-container">
-    <?php version9_post_thumbnail(); ?>
-  </div>
   
-  <div class="entry-content">
-    <?php
-      the_content();
-
-//      wp_link_pages( array(
-//        'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'version9' ),
-//        'after'  => '</div>',
-//      ) );
-    ?>
-
+  <div class="entry-content">    
+    <?php the_content(); ?>
+    
+    <p class="text-center screen-shot">
+      All screen shots were captured on <?php echo get_field( 'screenshot_date' ); ?>
+    </p>
+    
+    <?php if ( ! empty( get_field( 'website' ) ) ) { ?>
+      <p class="text-center font-weight-bold">
+        <a href="http://<?php echo get_field( 'website' ); ?>" target="_blank">
+          Click here to visit <?php echo the_title(); ?>
+        </a>
+      </p>
+    <?php } ?>
+    
     <footer class="entry-footer">
       <?php version9_post_footer_meta(); ?>
     </footer>
   </div>
 </article>
+
+<nav class="navigation post-navigation" role="navigation">
+  <h2 class="screen-reader-text">
+    Page navigation
+  </h2>
+  
+  <div class="nav-links">
+    <div class="nav-previous">
+      <a href="<?php echo AG_PORTFOLIO; ?>">
+        Back to Portfolio
+      </a>
+    </div>
+  </div>
+</nav>
