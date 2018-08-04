e2193dc2ffb49aeeb86e28d296b982c27281be43
Andrew Skrypnyk <a.g.skryp@gmail.com>
2018-08-04 16:38:28 -0400

    Update page content

--- page-template/page-about.php
+++ page-template/page-about.php
@@ -13,23 +13,23 @@
     
     <div class="entry-content">
       <p>
-        Hello, my name is Andrew Skrypnyk.  I am web designer &amp; developer from Toronto, Ontario, Canada.  I am passionate about writing clean and concise code, building beautiful and bug-free pages, and creating responsive, easy to navigate, user friendly websites.
+        Hello, my name is Andrew Skrypnyk.  I am web designer &amp; developer from Toronto, Ontario, Canada.  I am passionate about writing clean and concise code, building beautiful and bug-free pages, and creating responsive, easily navigatable, user friendly websites.
       </p>
 
       <p>
-        Since taking night class at a community college in 2013, I've been enhancing my skills on web development.  Learning the basics of HTML and CSS, I've taken my studies out of the classroom and moved them online.  Reading articles on the latest library, viewing other developer demos, and researching the best way of initializing scripts .  I'm continually keeping my tools and skills sharp.
+        Starting in January of 2013, I've taken night classes at a community college in 2013, learning the basics of HTML and CSS.  Since then I've been enhancing my skills on web development by taking my studies from the classroom and onto the internet.  Whether it's reading articles on the latest library, viewing and toying with another developers demo, or researching the best way of initializing scripts, I'm continually keeping my tools and skill set sharp.
       </p>
 
       <p>
-        My personal workstation is a simple MacBook Air.  Locally running a virtual machine, I do my coding in <a href="http://brackets.io/" target="_blank" rel="noopener">Brackets</a>.  Browser of choice is Chrome, but I do keep other major browsers around for testing.  I pull and push repo projects with GitKraken, and use FileZilla for uploading files to the web.  For any and all kinds of photos and image modifications, I play with Photoshop.
+        My personal workstation is a simple <a href="https://www.apple.com/ca/macbook-air/" target="_blank" rel="nofollow">MacBook Air</a>.  Locally running a <a href="https://www.vagrantup.com/" target="_blank" rel="nofollow">Vagrant's virtualbox</a>, I do my coding in <a href="http://brackets.io/" target="_blank" rel="noopener">Brackets</a>.  Browser of choice is <a href="https://www.google.com/chrome/" target="_blank" rel="nofollow">Google Chrome</a>, but I keep other major browsers around for testing.  I pull and push repo projects with <a href="https://www.gitkraken.com/" target="_blank" rel="nofollow">Axosoft's GitKraken</a>, and use <a href="https://filezilla-project.org/" target="_blank" rel="nofollow">FileZilla</a> for uploading files to the web.  For any and all kinds of photos and image modifications, I play with <a href="https://www.photoshop.com/" target="_blank" rel="nofollow">Adobe Photoshop</a>.
       </p>
 
       <p>
-        The purpose of this web site is to keep a record of and show off past projects I’ve been involved with.  From time to time, I’ll <a href="<?php echo esc_url( __( AG_BLOG, 'version9' ) ); ?>">blog</a> about my experience with these projects, write tutorials, deconstruct code, talk about something new I’ve come across, and anything else related to the world of the web.
+        The purpose of this web site is to keep a record of and show off <a href="<?php echo esc_url( __( AG_PORTFOLIO, 'version9' ) ); ?>">past projects</a> I’ve been involved with.  From time to time, I’ll <a href="<?php echo esc_url( __( AG_BLOG, 'version9' ) ); ?>">blog</a> about my experience with these projects, write tutorials, deconstruct code, talk about something new I’ve come across, or anything else that's related to the world of the web.
       </p>
 
       <p>
-        When I’m not lost in web building, I enjoy watching all types of cinema, listening to and producing a wide variety of music, drawing and writing comic strips, running or cycling when the weather is nice, solving sudoku / other puzzles, and starting a game of chess with queen’s gambit.
+        When I’m not lost in web building, I can found running or cycling when the weather is nice, listening to a variety of music, watching classic and contempoary films, reading on public transit, solving sudoku / other puzzles, and starting a game of chess with D2 - rD4.
       </p>
 
       <p class="text-center resume-link">
