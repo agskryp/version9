a4ecbcf2f347dc84dd11a802603abb41fd556384
Andrew Skrypnyk <a.g.skryp@gmail.com>
2019-02-20 12:52:11 -0500

    Code clean up

--- wp-content/themes/version9/inc/constants.php
+++ wp-content/themes/version9/inc/constants.php
@@ -1,19 +1,13 @@
 <?php
 
-  /**
-   * Assets
-   */
-  define( 'AG_RESUME', '/assets/ag_skryp_resume.pdf');
+// Assets
+define( 'AG_RESUME',    '/assets/ag_skryp_resume.pdf');
 
-  /**
-   * Pages 
-   */
-  define( 'AG_BLOG', '/blog' );
-  define( 'AG_PORTFOLIO', '/portfolio' );
-  define( 'AG_ABOUT', '/about' );
-  define( 'AG_PRIVACY', '/privacy-policy' );
+// Pages
+define( 'AG_BLOG',      '/blog' );
+define( 'AG_PORTFOLIO', '/portfolio' );
+define( 'AG_ABOUT',     '/about' );
+define( 'AG_PRIVACY',   '/privacy-policy' );
 
-  /**
-   * Version
-   */
-  define( 'AG_VERSION', '9.1.0' );
+// Version
+define( 'AG_VERSION',   '9.1.0' );
