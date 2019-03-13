8e089658a49db78048d685e7b78aacedbee308b8
Andrew Skrypnyk <a.g.skryp@gmail.com>
2019-03-13 12:23:48 -0400

    Update advanced custom fields plugin

---
+++ wp-content/mu-plugins/load-custom-fields.php
@@ -0,0 +1,15 @@
+<?php 
+
+/*
+Plugin Name: Advanced Custom Fields
+Plugin URI: https://www.advancedcustomfields.com/
+Description: Customize WordPress with powerful, professional and intuitive fields.
+Version: 5.7.12
+Author: Elliot Condon
+Author URI: http://www.elliotcondon.com/
+Copyright: Elliot Condon
+Text Domain: acf
+Domain Path: /lang
+*/
+
+require WPMU_PLUGIN_DIR . '/advanced-custom-fields/acf.php';
--- wp-content/mu-plugins/load.php
+++
@@ -1,4 +0,0 @@
-<?php 
-
-require WPMU_PLUGIN_DIR . '/classic-editor/classic-editor.php';
-require WPMU_PLUGIN_DIR . '/advanced-custom-fields/acf.php';
