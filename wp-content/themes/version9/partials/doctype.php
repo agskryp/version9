dfb7f87cd3c539f7811fe732e96cc7e28db02653
Andrew Skrypnyk <a.g.skryp@gmail.com>
2019-03-13 14:22:24 -0400

    Add google analytics script

--- wp-content/themes/version9/partials/doctype.php
+++ wp-content/themes/version9/partials/doctype.php
@@ -17,6 +17,15 @@
          });
     </script>
   <?php } ?>
+  
+  <script async src="https://www.googletagmanager.com/gtag/js?id=UA-136205780-1"></script>
+  <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'UA-136205780-1');
+  </script>
 </head>
 
 <body <?php body_class(); ?>>
