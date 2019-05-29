50d26f9e42cac9bedbdba0552bcfda4d6369977d
Andrew Skrypnyk <a.g.skryp@gmail.com>
2019-05-29 16:32:21 -0400

    Update Yoast SEO plugin

--- wp-content/plugins/wordpress-seo/vendor/autoload_52.php
+++ wp-content/plugins/wordpress-seo/vendor/autoload_52.php
@@ -4,4 +4,4 @@
 
 require_once dirname(__FILE__) . '/composer'.'/autoload_real_52.php';
 
-return ComposerAutoloaderInit8b3a89cbd6d6b8d784963573d27b4982::getLoader();
+return ComposerAutoloaderInit2d7f13f16500bfb2cf0e1067c3e65d2d::getLoader();
