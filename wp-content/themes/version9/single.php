811b774aae8277084ad743a8f71ebff382091772
Andrew Skrypnyk <a.g.skryp@gmail.com>
2019-08-29 15:13:36 -0400

    Remove google ads on singles template, don't have blog nav on portfolio

--- wp-content/themes/version9/single.php
+++ wp-content/themes/version9/single.php
@@ -18,18 +18,13 @@
         if( get_post_type() === 'portfolio' ) get_template_part( 'template-parts/content', 'portfolio' );
         else get_template_part( 'template-parts/content', get_post_type() );
     ?>
-      <div class="ad-container">
-        <ins class="adsbygoogle" style="display:block"
-             data-ad-client="ca-pub-5942635838820429" data-ad-slot="8057614268"
-             data-ad-format="auto" data-full-width-responsive="true">
-        </ins>
 
-        <script>(adsbygoogle = window.adsbygoogle || []).push({});</script>
-      </div>
     <?php
-        if( comments_open() || get_comments_number() ) comments_template();
+        if( comments_open() || get_comments_number() ) {
+          comments_template();
 
-        version9_blog_navigation();
+          version9_blog_navigation();
+        }
       }
 	?>
   </main>
