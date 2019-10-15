f9e65d19f7264f6ab4208f34c71ae34e6a142f09
Andrew Skrypnyk <a.g.skryp@gmail.com>
2019-10-15 12:24:32 -0400

    Don't load navigation.js on front page

--- wp-content/themes/version9/functions.php
+++ wp-content/themes/version9/functions.php
@@ -133,9 +133,11 @@ function version9_scripts() {
   );  
 
   // JS
-  wp_enqueue_script( 
-    'version9-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20151215', true
-  );
+  if( !is_front_page() ) {
+    wp_enqueue_script( 
+      'version9-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20151215', true
+    );
+  }
   
   wp_enqueue_script( 
     'version9-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20151215', true
