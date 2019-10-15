6d8755f8141d06dc55f5e822f80e40677b072193
Andrew Skrypnyk <a.g.skryp@gmail.com>
2019-10-15 13:32:20 -0400

    Minimize bootstrap wip

--- wp-content/themes/version9/functions.php
+++ wp-content/themes/version9/functions.php
@@ -120,14 +120,14 @@ add_action( 'widgets_init', 'version9_widgets_init' );
 function version9_scripts() {
   
   // CSS
-  wp_enqueue_style( 
-    'custom-google-fonts', 'https://fonts.googleapis.com/css?family=Lato:100,300,700'
-  );
-  
-  wp_enqueue_style(
-    'bootstrap-v4.1', get_template_directory_uri() . '/css/bootstrap.min.css', array(), '4.1.0', false 
-  );
-  
+//  wp_enqueue_style( 
+//    'custom-google-fonts', ''
+//  );
+//  
+//  wp_enqueue_style(
+//    'bootstrap-v4.1', get_template_directory_uri() . '/css/bootstrap.min.css', array(), '4.1.0', false 
+//  );
+//  
   wp_enqueue_style( 
     'version9-style', get_stylesheet_uri(), array(), AG_VERSION, false 
   );  
@@ -190,3 +190,23 @@ function new_excerpt( $more ) {
   return '... <br> <a class="read-more pull-left" href="' . get_permalink( $post -> ID ) . '"> Continue reading &rarr;</a>';
 }
 add_filter( 'excerpt_more', 'new_excerpt' );
+
+
+
+
+
+
+
+add_action( 'wp_head', 'themeprefix_load_fonts' ); 
+function themeprefix_load_fonts() { 
+    ?> 
+<!-- Code snippet to speed up Google Fonts rendering: googlefonts.3perf.com --> 
+<link rel="dns-prefetch" href="https://fonts.gstatic.com"> 
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin="anonymous"> 
+<link rel="preload" href="https://fonts.googleapis.com/css?family=Lato:100,300,700" as="fetch" crossorigin="anonymous"> 
+<script type="text/javascript"> 
+!function(e,n,t){"use strict";var o="https://fonts.googleapis.com/css?family=Lato:100,300,700",r="__3perf_googleFontsStylesheet";function c(e){(n.head||n.body).appendChild(e)}function a(){var e=n.createElement("link");e.href=o,e.rel="stylesheet",c(e)}function f(e){if(!n.getElementById(r)){var t=n.createElement("style");t.id=r,c(t)}n.getElementById(r).innerHTML=e}e.FontFace&&e.FontFace.prototype.hasOwnProperty("display")?(t[r]&&f(t[r]),fetch(o).then(function(e){return e.text()}).then(function(e){return e.replace(/@font-face {/g,"@font-face{font-display:swap;")}).then(function(e){return t[r]=e}).then(f).catch(a)):a()}(window,document,localStorage); 
+</script>
+<!-- End of code snippet for Google Fonts -->
+    <?php
+}
