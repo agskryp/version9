a9dfb8bf6b6277b8a2c38d4a5f79c844184702fa
Andrew Skrypnyk <a.g.skryp@gmail.com>
2019-10-15 14:48:23 -0400

    Kill bootstrap, update + host TweenMax, code clean up

--- wp-content/themes/version9/functions.php
+++ wp-content/themes/version9/functions.php
@@ -120,14 +120,6 @@ add_action( 'widgets_init', 'version9_widgets_init' );
 function version9_scripts() {
   
   // CSS
-//  wp_enqueue_style( 
-//    'custom-google-fonts', ''
-//  );
-//  
-//  wp_enqueue_style(
-//    'bootstrap-v4.1', get_template_directory_uri() . '/css/bootstrap.min.css', array(), '4.1.0', false 
-//  );
-//  
   wp_enqueue_style( 
     'version9-style', get_stylesheet_uri(), array(), AG_VERSION, false 
   );  
@@ -145,7 +137,7 @@ function version9_scripts() {
   
   if( is_front_page() ) {
     wp_enqueue_script( 
-      'tween-max-v1.19', 'https://cdnjs.cloudflare.com/ajax/libs/gsap/1.19.0/TweenMax.min.js', array(), '1.19.0', true
+      'tween-max-v2.1.3', get_template_directory_uri() . '/js/TweenMax.min.js', array(), '2.1.3', true
     );    
     
     wp_enqueue_script( 
@@ -196,21 +188,23 @@ add_filter( 'excerpt_more', 'new_excerpt' );
 
 
 
+/**
+ * Best embedding of google fonts
+ */
 
-add_action( 'wp_head', 'themeprefix_load_fonts' ); 
 function themeprefix_load_fonts() { 
-    ?> 
-<!-- Code snippet to speed up Google Fonts rendering: googlefonts.3perf.com --> 
-<link rel="dns-prefetch" href="https://fonts.gstatic.com"> 
-<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin="anonymous"> 
-<link rel="preload" href="https://fonts.googleapis.com/css?family=Lato:100,300,700" as="fetch" crossorigin="anonymous"> 
-<script type="text/javascript"> 
-!function(e,n,t){"use strict";var o="https://fonts.googleapis.com/css?family=Lato:100,300,700",r="__3perf_googleFontsStylesheet";function c(e){(n.head||n.body).appendChild(e)}function a(){var e=n.createElement("link");e.href=o,e.rel="stylesheet",c(e)}function f(e){if(!n.getElementById(r)){var t=n.createElement("style");t.id=r,c(t)}n.getElementById(r).innerHTML=e}e.FontFace&&e.FontFace.prototype.hasOwnProperty("display")?(t[r]&&f(t[r]),fetch(o).then(function(e){return e.text()}).then(function(e){return e.replace(/@font-face {/g,"@font-face{font-display:swap;")}).then(function(e){return t[r]=e}).then(f).catch(a)):a()}(window,document,localStorage); 
-</script>
-<!-- End of code snippet for Google Fonts -->
-    <?php
+  ?> 
+    <!-- Code snippet to speed up Google Fonts rendering: googlefonts.3perf.com --> 
+    <link rel="dns-prefetch" href="https://fonts.gstatic.com"> 
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin="anonymous"> 
+    <link rel="preload" href="https://fonts.googleapis.com/css?family=Lato:100,300,700" as="fetch" crossorigin="anonymous"> 
+    <script type="text/javascript"> 
+    !function(e,n,t){"use strict";var o="https://fonts.googleapis.com/css?family=Lato:100,300,700",r="__3perf_googleFontsStylesheet";function c(e){(n.head||n.body).appendChild(e)}function a(){var e=n.createElement("link");e.href=o,e.rel="stylesheet",c(e)}function f(e){if(!n.getElementById(r)){var t=n.createElement("style");t.id=r,c(t)}n.getElementById(r).innerHTML=e}e.FontFace&&e.FontFace.prototype.hasOwnProperty("display")?(t[r]&&f(t[r]),fetch(o).then(function(e){return e.text()}).then(function(e){return e.replace(/@font-face {/g,"@font-face{font-display:swap;")}).then(function(e){return t[r]=e}).then(f).catch(a)):a()}(window,document,localStorage); 
+    </script>
+    <!-- End of code snippet for Google Fonts -->
+  <?php
 }
-
+add_action( 'wp_head', 'themeprefix_load_fonts' ); 
 
 
 // Deregisters contact form 7 script unless on about page
