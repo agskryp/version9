eafc408cda72ac4fcb05e6d3f1ac92db3442c2a7
Andrew Skrypnyk <a.g.skryp@gmail.com>
2019-10-17 11:22:38 -0400

    Style update

--- wp-content/themes/version9/search.php
+++ wp-content/themes/version9/search.php
@@ -11,7 +11,7 @@
 
 <div id="primary">
   <main id="main" class="site-main search-page excerpt-page">
-    <?php if ( have_posts() ) : ?>
+    <?php if( have_posts() ) : ?>
       <header class="page-header">
         <h1 class="page-title text-center">
           <?php
@@ -34,7 +34,7 @@
 
       <?php
         /* Start the Loop */
-        while ( have_posts() ) :
+        while( have_posts() ) :
           the_post();
 
           /**
