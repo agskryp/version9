2008ab9c1b2bfe522ac1c27bd9f19496c649229c
Andrew Skrypnyk <a.g.skryp@gmail.com>
2020-01-22 12:14:19 -0500

    Remove 9.2.4 theme

--- wp-content/themes/version9-2-4/searchform.php
+++
@@ -1,9 +0,0 @@
-<form role="search" method="get" class="search-form" action="<?php echo home_url( '/' );  ?>" aria-expanded="false">
-  <label>
-    <span class="screen-reader-text">Search for:</span>
-
-    <input type="search" class="search-field" placeholder="Search" name="s" title="Search for:" />
-  </label>
-
-  <input type="submit" class="search-submit" value="Go" />
-</form>
