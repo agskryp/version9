12171a89d243f42df74bee667bd772397b102ff5
Andrew Skrypnyk <a.g.skryp@gmail.com>
2020-01-28 10:23:36 -0500

    Add agskrypv4

---
+++ wp-content/themes/version9/assets/themes/agskryp/agskrypv5/js/agsheet.js
@@ -0,0 +1,47 @@
+var AG5app = angular.module('agv5', ["ui.router"]);
+
+AG5app.config(function($stateProvider, $urlRouterProvider){
+	$urlRouterProvider.when('', '/');
+	$urlRouterProvider.otherwise("/");
+	$stateProvider
+		.state('home', {
+    		url: "/",
+			templateUrl: "../partials/home.html"
+    	})
+		.state('work', {
+    		url: "/work",
+    		templateUrl: "../partials/work.html"
+    	})
+    	.state('about', {
+			url: "/about",
+    		templateUrl: "../partials/about.html"
+    	})
+		.state('credit', {
+			url: "/credit",
+			templateUrl: "../partials/credits.html"
+    	});
+});
+
+AG5app.controller('ScrollController', ['$scope', '$location', '$anchorScroll',
+  function ($scope, $location, $anchorScroll) {
+    $scope.gotoEarliest = function() {
+      $location.hash('earliestWork');
+	  $anchorScroll();
+    };
+	$scope.gotoLatest = function() {
+      $location.hash('latestProject');
+	  $anchorScroll();
+    };
+	$scope.gotoAbout = function() {
+      $location.hash('aboutMe');
+	  $anchorScroll();
+    };
+	$scope.gotoContact = function() {
+      $location.hash('contactMe');
+	  $anchorScroll();
+    };
+  }]);
+
+function windowReset(){
+		window.scrollTo(0,0);
+	}
