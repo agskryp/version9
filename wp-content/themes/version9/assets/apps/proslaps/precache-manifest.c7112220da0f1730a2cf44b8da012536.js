f9a3aabbcdc2bd64bb80d8d0de1854fb4c76dfbc
Andrew Skrypnyk <a.g.skryp@gmail.com>
2020-03-22 16:24:44 -0400

    Import ProSlaps app

---
+++ wp-content/themes/version9/assets/apps/proslaps/precache-manifest.c7112220da0f1730a2cf44b8da012536.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d47a9f3440dcf1cb0cdb",
+    "url": "css/app.9ce8b615.css"
+  },
+  {
+    "revision": "bee1c6ab6ad54a6a82bf",
+    "url": "css/chunk-01ab6b48.abec31fd.css"
+  },
+  {
+    "revision": "8776d34b7aff035ab417",
+    "url": "css/chunk-3d0a10d4.abec31fd.css"
+  },
+  {
+    "revision": "bd8317dd4c45f75eda2b",
+    "url": "css/chunk-43548bd5.abec31fd.css"
+  },
+  {
+    "revision": "73034efb99288948bf2c6ccd92d7508b",
+    "url": "index.html"
+  },
+  {
+    "revision": "d47a9f3440dcf1cb0cdb",
+    "url": "js/app.6f261b10.js"
+  },
+  {
+    "revision": "bee1c6ab6ad54a6a82bf",
+    "url": "js/chunk-01ab6b48.7983c8a4.js"
+  },
+  {
+    "revision": "8776d34b7aff035ab417",
+    "url": "js/chunk-3d0a10d4.bc778954.js"
+  },
+  {
+    "revision": "bd8317dd4c45f75eda2b",
+    "url": "js/chunk-43548bd5.662a4827.js"
+  },
+  {
+    "revision": "f1ef12d9075e9b2aee4e",
+    "url": "js/npm.core-js.6210e9de.js"
+  },
+  {
+    "revision": "75df18ab4934fe6cad24",
+    "url": "js/npm.fortawesome.54b41f8d.js"
+  },
+  {
+    "revision": "af441b0912aad7f2ca00",
+    "url": "js/npm.register-service-worker.17db0562.js"
+  },
+  {
+    "revision": "bcc61034111612589940",
+    "url": "js/npm.vue-loader.edcedc37.js"
+  },
+  {
+    "revision": "3af79f39d8345082cf4a",
+    "url": "js/npm.vue.f3cd6edb.js"
+  },
+  {
+    "revision": "796117e36026635f3cce",
+    "url": "js/npm.webpack.fab97803.js"
+  },
+  {
+    "revision": "dd5631c20f6c8517f5c8",
+    "url": "js/runtime.b22c7815.js"
+  },
+  {
+    "revision": "0092e2c9173d3214396ac31f2955c650",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  }
+]);
