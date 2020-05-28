ff25c3423aaa394fa99b793a2ede80938d3c8e31
Andrew Skrypnyk <a.g.skryp@gmail.com>
2020-05-27 22:28:17 -0400

    Install CMB2 as a mu-plugin

---
+++ wp-content/mu-plugins/load-cmb2.php
@@ -0,0 +1,27 @@
+<?php 
+/**
+ * @category     WordPress_Plugin
+ * @package      CMB2
+ * @author       CMB2 team
+ * @license      GPL-2.0+
+ * @link         https://cmb2.io
+ *
+ * Plugin Name:  CMB2
+ * Plugin URI:   https://github.com/CMB2/CMB2
+ * Description:  CMB2 will create metaboxes and forms with custom fields that will blow your mind.
+ * Author:       CMB2 team
+ * Author URI:   https://cmb2.io
+ * Contributors: Justin Sternberg (@jtsternberg / dsgnwrks.pro)
+ *               WebDevStudios (@webdevstudios / webdevstudios.com)
+ *               Human Made (@humanmadeltd / hmn.md)
+ *               Jared Atchison (@jaredatch / jaredatchison.com)
+ *               Bill Erickson (@billerickson / billerickson.net)
+ *               Andrew Norcross (@norcross / andrewnorcross.com)
+ *
+ * Version:      2.7.0
+ *
+ * Text Domain:  cmb2
+ * Domain Path:  languages
+ */
+
+require WPMU_PLUGIN_DIR . '/cmb2/init.php';
