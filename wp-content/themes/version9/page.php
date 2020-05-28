196e48500f0da816d5d972449c6d618a0ff9923c
Andrew Skrypnyk <a.g.skryp@gmail.com>
2020-05-27 23:01:42 -0400

    Move content-page, default page template wip

--- wp-content/themes/version9/page.php
+++ wp-content/themes/version9/page.php
@@ -1,33 +1,57 @@
 <?php
   /**
-   * The template for displaying all pages
-   *
-   * This is the template that displays all pages by default.
-   * Please note that this is the WordPress construct of pages
-   * and that other 'pages' on your WordPress site may use a
-   * different template.
-   *
-   * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
-   * @package version9
+   * Template Name: Default Template 
    */
 
   get_header();
 ?>
 
-<div id="primary" class="content-area">
+<div>
   <main id="main" class="site-main page-page content-page">
     <?php
-      while ( have_posts() ) :
+      while( have_posts() ) {
         the_post();
+    ?>
+      <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+        <header class="entry-header">
+          <?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+        </header>
 
-        get_template_part( 'template-parts/content', 'page' );
+        <div class="entry-content">
+          <?php
+            the_content();
+
+            wp_link_pages( array(
+          'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'version9' ),
+          'after'  => '</div>',
+            ) );
+	
+      if( get_edit_post_link() ) :
+    ?>      
+      <footer class="entry-footer">
+		<?php
+		  edit_post_link( sprintf( wp_kses(
+		    /* translators: %s: Name of current post. Only visible to screen readers */
+            __( 'Edit <span class="screen-reader-text">%s</span>', 'version9' ),
+            array(
+              'span' => array(
+                'class' => array(),
+            ), )
+		  ), get_the_title() ),	'<span class="edit-link">',	'</span>' );
+		?>
+      </footer>
+	<?php endif; ?>
+  </div>
+</article>
+
+        <?php
 
         // If comments are open or we have at least one comment, load up the comment template.
         if ( comments_open() || get_comments_number() ) :
           comments_template();
         endif;
 
-      endwhile; // End of the loop.
+      }
     ?>
   </main>
   
