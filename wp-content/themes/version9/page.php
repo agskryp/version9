fda0dad82706088eabe11f0c344848a8fb805926
Andrew Skrypnyk <a.g.skryp@gmail.com>
2020-05-28 22:35:23 -0400

    Simplify wip

--- wp-content/themes/version9/page.php
+++ wp-content/themes/version9/page.php
@@ -13,11 +13,11 @@
         the_post();
     ?>
       <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-        <header class="entry-header">
-          <?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+        <header class="page-header">
+          <?php the_title( '<h1 class="page-title">', '</h1>' ); ?>
         </header>
 
-        <div class="entry-content">
+        <div class="page-content">
           <?php the_content(); ?>
         </div>
       </article>
