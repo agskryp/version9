0c11f7a6d49b4f4801f7b8f4b838ac80e7c28674
Andrew Skrypnyk <a.g.skryp@gmail.com>
2020-05-30 15:33:09 -0400

    Update

--- wp-content/themes/version9/single-portfolio.php
+++ wp-content/themes/version9/single-portfolio.php
@@ -14,17 +14,17 @@
 ?>
 
 <div>
-  <main>
+  <main class="portfolio-post-container">
     <?php
-	    while( have_posts() ) {
+      while( have_posts() ) {
         the_post();
     ?>
       <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
         <header class="page-header">
           <?php the_title( '<h1 class="page-title">', '</h1>' ); ?>
         </header>
-  
-        <div class="page-content portfolio-post"> 
+
+        <div class="page-content"> 
           <?php the_content(); ?>
     
           <div class="text-center">
@@ -46,6 +46,6 @@
       </nav>
     <?php } ?>
   </main>
-  
+
   <?php get_footer(); ?>
 </div>
