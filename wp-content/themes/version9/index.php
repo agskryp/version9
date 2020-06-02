d1898b7a8380e622d058d2e58980db6e4ceb4c9d
Andrew Skrypnyk <a.g.skryp@gmail.com>
2020-06-01 23:11:36 -0400

    Index page wip

--- wp-content/themes/version9/index.php
+++ wp-content/themes/version9/index.php
@@ -7,7 +7,7 @@
 ?>
 
 <div>
-  <main class="site-main index-page excerpt-page">
+  <main class="index-page-container">
     <header class="page-header">
       <h1 class="page-title"><?php single_post_title(); ?></h1>
     </header>
@@ -16,14 +16,27 @@
       if( have_posts() ) {
         while( have_posts() ) {
           the_post();
-
-          get_template_part( 'template-parts/content', get_post_type() );
+      ?>
+        <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+          <header class="entry-header">
+            <h2 class="entry-title">
+              <a href="<?php echo esc_url( get_permalink() ); ?>" rel="bookmark"><?php the_title(); ?></a>
+            </h2>
+              
+            <div class="entry-meta">
+              <?php version9_post_header_meta(); ?>
+            </div>
+          </header>
+  
+          <div class="entry-content">
+            <?php the_excerpt(); ?>
+          </div>
+        </article>
+      <?php
         }
 
         version9_posts_navigation();
       }
-    
-      else get_template_part( 'template-parts/content', 'none' );  
     ?>   
     
     <div class="ad-container">
