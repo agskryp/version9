d183324f83ec6d94aa2612dcf4f1283def35828c
Andrew Skrypnyk <a.g.skryp@gmail.com>
2020-06-02 22:40:35 -0400

    Index Page wip

--- wp-content/themes/version9/index.php
+++ wp-content/themes/version9/index.php
@@ -19,13 +19,13 @@
       ?>
         <article id="post-<?php the_ID(); ?>" <?php post_class( 'page-content' ); ?>>
           <header class="entry-header">
-            <h2 class="entry-title">
-              <a href="<?php echo esc_url( get_permalink() ); ?>" rel="bookmark"><?php the_title(); ?></a>
-            </h2>
-              
             <div class="entry-meta">
-              <?php version9_post_header_meta(); ?>
+              <?php echo get_the_category_list( ', ' ); ?>
             </div>
+
+            <h2 class="entry-title">
+              <a href="<?php echo esc_url( get_permalink() ); ?>"><?php the_title(); ?></a>
+            </h2>
           </header>
   
           <div class="entry-content">
