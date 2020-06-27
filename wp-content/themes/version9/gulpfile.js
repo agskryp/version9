1b2342d8b5d3feb752276c8a43089194bbdf46b5
Andrew Skrypnyk <a.g.skryp@gmail.com>
2020-06-27 15:46:11 -0400

    Add autoprefix to css

--- wp-content/themes/version9/gulpfile.js
+++ wp-content/themes/version9/gulpfile.js
@@ -1,17 +1,19 @@
 'use strict';
  
-var gulp = require('gulp');
-var sass = require('gulp-sass');
+const gulp         = require('gulp');
+const sass         = require('gulp-sass');
+const autoprefixer = require('gulp-autoprefixer');
   
 sass.compiler = require('node-sass');
   
 gulp.task('sass', function () {
   return gulp.src('./sass/**/*.scss')
     .pipe(sass.sync({outputStyle: 'compressed'}).on('error', sass.logError))
+    // .pipe(sass.sync().on('error', sass.logError))
+    .pipe(autoprefixer({cascade: false}))
     .pipe(gulp.dest('./'));
 });
 
 gulp.task('watch', function() {
-  gulp.watch('./old-sass/**/*.scss', gulp.series('sass'));
   gulp.watch('./sass/**/*.scss', gulp.series('sass'));
 });
